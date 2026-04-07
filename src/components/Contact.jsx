7af091e3b406c151386e0660ecf908ca40d0d06a
MalairamanC <malairmn@gmail.com>
2026-04-07 11:15:28 +0530

    Update Contact.jsx

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -86,7 +86,7 @@ function Contact() {
               <div className="space-y-4">
                 <div className="flex items-center gap-3">
                   <Mail className="text-purple-400" size={22} />
-                  <a href="mailto:info@jaswisys.com" className="hover:text-purple-400">info@jaswisys.com</a>
+                  <a href="mailto:info@jaswisys.com" className="hover:text-purple-400">jaswisys@gmail.com</a>
                 </div>
 
                 <div className="flex items-center gap-3">
